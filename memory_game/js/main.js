eef195480a6373e044a4470b82e63d0b702e9928
Indiana Kuffer <indianakuffer@gmail.com>
2020-03-30 19:47:12 -0400

    game js changes

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-console.log('Up and running!');
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log('User flipped ' + cardThree);
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardTwo);
+
+if(cardsInPlay.length === 2) {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again!");
+  }
+  cardsInPlay = [];
+}
